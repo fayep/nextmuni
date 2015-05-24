03db7270ce342009cafb2f6159f21c36860b913d
Faye Salwin <faye.salwin@betfair.com>
2015-05-23 19:28:17 -0700

    now locations

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -1,5 +1,20 @@
+var locationOptions = {
+  enableHighAccuracy: true,
+  maximumAge: 10000,
+  timeout: 10000
+};
+
+function locationSuccess(pos) {
+  console.log('lat= ' + pos.coords.latitude + ' lon= ' + pos.coords.longitude);
+}
+
+function locationError(err) {
+  console.log('location error (' + err.code + '): ' + err.message);
+}
+
 Pebble.addEventListener("ready",
-    function(e) {
-        console.log("Hello world! - Sent from your javascript application.");
-    }
+  function(e) {
+    console.log("Hello world! - Sent from your javascript application.");
+    navigator.geolocation.getCurrentPosition(locationSuccess, locationError, locationOptions);
+  }
 );
