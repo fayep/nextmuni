b1398a520d762859172156e4a117d74e419ce8e0
Faye Salwin <faye.salwin@betfair.com>
2015-05-23 20:51:00 -0700

    debug logging

--- src/locateme.c
+++ src/locateme.c
@@ -23,7 +23,7 @@ static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
 static void process_dictionary(DictionaryIterator *iterator) {
   Tuple *t = dict_read_first(iterator);
   while (t != NULL) {
-    APP_LOG(APP_LOG_LEVEL_DEBUG, "%d:%s", t->key, t->value->cstring);
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "%lu:%s", t->key, t->value->cstring);
     switch (t->key) {
       case KEY_LATITUDE:
         strncpy(t->value->cstring, latitude, 9);
