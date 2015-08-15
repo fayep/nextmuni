d8bf8d7b9a30e867e779d002059ddc69f350a611
fayep <faye.salwin@opower.com>
2015-08-15 13:57:57 -0700

    I think Cloudpebble is broken.

--- src/app.js
+++ src/app.js
@@ -1,46 +1,61 @@
 var UI = require('ui');
+var ajax = require('ajax');
 
-var card = new UI.Card({
-  title:'Next Muni',
-  subtitle:'Fetching...'
+
+var routes;
+var stops;
+var card;
+
+function onNearestOK(data, status, request) {
+  console.log('loaded data');
+  card.subtitle('Parsing...');
+  card.show();
+  routes = data.routes;
+  stops = data.stops;
+  console.log('stops: '+stops.toString());
+}
+
+function onNearestError(error, status, request) {
+  card.subtitle('Failed.');
+  card.body(error);
+  card.show();
+}
+
+function onLocationOK(pos) {
+  console.log('loading data');
+  card.subtitle('Loading...');
+  card.show();
+  ajax(
+    {
+      url: 'http://hosted.zippysoft.com/nearest/sf-muni/'+pos.coords.latitude.toString()+'/'+pos.coords.longitude.toString(),
+      type: 'json'
+    }, onNearestOK, onNearestError);
+}
+
+function onLocationError(err) {
+  console.log('location error (' + err.code + '): ' + err.message);
+  card.subtitle('Failed.');
+  card.body('Failed to locate you.');
+  card.show();
+}
+
+function onAppReady(e) {
+  console.log('fetching location');
+  card.subtitle('Fetching...');
+  card.show();
+  navigator.geolocation.getCurrentPosition(
+    onLocationOK, onLocationError,
+    {
+      enableHighAccuracy: true,
+      maximumAge: 10000,
+      timeout: 10000
+    });
+}
+
+card = new UI.Card({
+  title: 'Next Muni',
+  subtitle: 'Starting...'
 });
 
-// Display the Card
+Pebble.addEventListener('ready', onAppReady);
 card.show();
-
-var locationOptions = {
-  enableHighAccuracy: true,
-  maximumAge: 10000,
-  timeout: 10000
-};
-
-var xhrRequest = function (url, type, callback) {
-  var xhr = new XMLHttpRequest();
-  xhr.onload = function () {
-    callback(this.responseText);
-  };
-  xhr.open(type, url);
-  xhr.send();
-};
-
-function locationSuccess(pos) {
-  var url = 'http://hosted.zippysoft.com/nearest/sf-muni/'+pos.coords.latitude.toString()+'/'+pos.coords.longitude.toString();
-  console.log(url);
-  xhrRequest(url, 'GET', function(response) {
-    var json = JSON.parse(response);
-    console.log("Callback\n"+response);
-    Pebble.sendAppMessage({'location': json.stops[0].title});
-  });
-}
-
-
-function locationError(err) {
-  console.log('location error (' + err.code + '): ' + err.message);
-}
-
-Pebble.addEventListener("ready",
-  function(e) {
-    console.log("Hello world! - Sent from your javascript application.");
-    navigator.geolocation.getCurrentPosition(locationSuccess, locationError, locationOptions);
-  }
-);
