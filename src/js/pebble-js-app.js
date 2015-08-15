c6ac0660a0e9f696c856f27f99bc2bbab22d460e
Faye Salwin <faye.salwin@opower.com>
2015-08-15 09:36:15 -0700

    testing

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -4,10 +4,23 @@ var locationOptions = {
   timeout: 10000
 };
 
+var xmlhttp = new XMLHttpRequest();
+
+xmlhttp.onreadystatechange = function() {
+if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+    var response = JSON.parse(xmlhttp.responseText);
+    xmlhttp.callback(response);
+    }
+}
+
+function nextBusCallback(response) {
+  Pebble.sendAppMessage({'latitude': response.stops[0].title, 'longitude': ''});
+}
+
 function locationSuccess(pos) {
-  console.log('lat= ' + pos.coords.latitude + ' lon= ' + pos.coords.longitude);
-  Pebble.sendAppMessage({'latitude': pos.coords.latitude.toString(),
-                         'longitude': pos.coords.longitude.toString()});
+  var url = 'http://hosted.zippysoft.com/nearest/sf-muni/'+pos.coords.latitude.toString()+'/'+pos.coords.longitude.toString();
+  xmlhttp.callback = nextBusCallback;
+  xmlhttp.open('GET', url, true);
 }
 
 function locationError(err) {
