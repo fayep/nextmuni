6d359d5a0890c2a8f3943b8632fa1a381cf2922b
fayep <faye.salwin@opower.com>
2015-08-15 11:19:57 -0700

    First

---
+++ src/js/pebble-js-app.js
@@ -0,0 +1,36 @@
+var locationOptions = {
+  enableHighAccuracy: true,
+  maximumAge: 10000,
+  timeout: 10000
+};
+
+var xhrRequest = function (url, type, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.onload = function () {
+    callback(this.responseText);
+  };
+  xhr.open(type, url);
+  xhr.send();
+};
+
+function locationSuccess(pos) {
+  var url = 'http://hosted.zippysoft.com/nearest/sf-muni/'+pos.coords.latitude.toString()+'/'+pos.coords.longitude.toString();
+  console.log(url);
+  xhrRequest(url, 'GET', function(response) {
+    var json = JSON.parse(response);
+    console.log("Callback\n"+response);
+    Pebble.sendAppMessage({'location': json.stops[0].title});
+  });
+}
+
+
+function locationError(err) {
+  console.log('location error (' + err.code + '): ' + err.message);
+}
+
+Pebble.addEventListener("ready",
+  function(e) {
+    console.log("Hello world! - Sent from your javascript application.");
+    navigator.geolocation.getCurrentPosition(locationSuccess, locationError, locationOptions);
+  }
+);
